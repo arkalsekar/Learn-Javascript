c4c7032e5a91756fc7114d2c87f381a89075a176
Alex Ventura <me@alexventura.dev>
2022-10-04 14:12:32 -0500

    Add time measuring example.

--- Tutorials/01_Console/01_Console.js
+++ Tutorials/01_Console/01_Console.js
@@ -1,4 +1,4 @@
-// Everything about Console 
+// Everything about Console
 // Console is like a print from Python
 
 console.log("hello World")
@@ -6,7 +6,7 @@ console.error("Hello Wrold ")
 console.warn("Hello Wrold ")
 
 // Clears the Previous Consoles and Prints
-console.clear() 
+console.clear()
 
 
 // Only Displays Alert Boxes
@@ -14,3 +14,29 @@ console.clear()
 
 // Displays and Alert as well as an Input Field
 console.prompt("Please Enter your Age")
+
+
+
+/* Measuring how much time does an algorith takes to execute
+ * In the browser console we have these two methods really handy to measure execution time:
+ * console.time(_label_);
+ * console.timeEnd(_label_);
+ */
+
+// We should give the timer a label identifier to be able to stop it later.
+console.time('for-loop');
+for (let i = 0; i < 1000000; i++) {
+  // some code
+}
+console.timeEnd('for-loop');
+
+let i = 0;
+console.time('while-loop');
+while (i < 1000000) {
+  i++
+}
+console.timeEnd('while-loop');
+
+// Expected output:
+// for-loop: 1.703125 ms
+// while-loop: 1.0322265625 ms
