9b8ca45b94d5c65c31cbd19e831167053da13c69
Mohd Salman <salman0ansariii@gmail.com>
2022-10-04 22:47:01 +0530

    Update 39.In_Keyword.js

--- Tutorials/39.In_Keyword.js
+++ Tutorials/39.In_Keyword.js
@@ -1,8 +1,8 @@
-// The keyword in is used to determine whether a property exists in an object. 
-// We use it in a for..in loop without understanding it was a keyword:)
+// The keyword "in" is used to determine whether a property exists in an object. 
+// We use it in a "for..in" loop without understanding it was a keyword "_"
 
-// If an object has a property, in will // return true; 
-// otherwise, it will // return false.
+// If an object has a property, in will return // true; 
+// otherwise, it will return // false.
 
 const human = {
     brain: "yes"
